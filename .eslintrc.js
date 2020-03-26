145428b0dbb3ca668ab057b0babeabeb2980a3d2
Alexander <alex@alex.media>
2020-03-26 18:12:07 -0400

    Changed Parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
         node: true
     },
     extends: ["plugin:vue/base", "plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-    parserOptions: {
-        parser: "babel-eslint"
-    },
+    parser: "vue-eslint-parser",
+    // parserOptions: {
+    //     parser: "babel-eslint"
+    // },
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
