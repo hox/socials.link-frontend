bf7833b2c19cc4f72c8120349ca0618dcc33030a
Alexander <alex@alex.media>
2020-03-25 22:21:29 -0400

    Configured SASS for .scss files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  css: {
-    requireModuleExtension: true
-  }
+    css: {
+        loaderOptions: {
+            sass: {
+                prependData: `@import "@/assets/SCSS/variables.scss"`
+            }
+        }
+    }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,14 @@
 module.exports = {
-  mode: "development",
-  entry: "src/main.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].[chunkhash:8].js"
-  },
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "eslint-loader"
-      },
-      {
-        test: /\.vue$/,
-        exclude: /node_modules/,
-        use: "vue-loader"
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              indentedSyntax: true
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
-          }
         ]
-      }
-    ]
-  }
+    }
 };
