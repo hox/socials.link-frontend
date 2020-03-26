8913cf0980d4948027b8df80f6801dc9ea465431
Alexander <alex@alex.media>
2020-03-26 01:29:52 -0400

    Moved Landing.vue and renamed components

---
+++ src/pages/Landing/Landing.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <LandingHeader />
+        <LandingIntegration />
+        <LandingTestimonies />
+        <LandingPlans />
+        <LandingFooter />
+    </div>
+</template>
+
+<script>
+    import LandingHeader from "./LandingHeader.vue";
+    import LandingIntegration from "./LandingIntegration.vue";
+    import LandingTestimonies from "./LandingTestimonies.vue";
+    import LandingPlans from "./LandingPlans.vue";
+    import LandingFooter from "./LandingFooter.vue";
+
+    export default {
+        name: "Landing",
+        components: {
+            LandingHeader,
+            LandingIntegration,
+            LandingTestimonies,
+            LandingPlans,
+            LandingFooter
+        }
+    };
+</script>
+
+<style lang="scss" scoped></style>
